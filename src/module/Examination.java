a9d19effa9842956a018bdd5046fee4b5b5d564f
Grace <lunay0325@gmail.com>
2019-04-21 19:28:44 +0800

    modify the parameter and override the toString function

--- src/module/Examination.java
+++ src/module/Examination.java
@@ -5,6 +5,15 @@ public class Examination {
     public Subject subject;
     public int duration;
     public int totalScore;
+    public String name;
+
+    public Examination(int examinationId, Subject subject, int duration, int totalScore, String name) {
+        this.examinationId = examinationId;
+        this.subject = subject;
+        this.duration = duration;
+        this.totalScore = totalScore;
+        this.name = name;
+    }
 
     public int getExaminationId() {
         return this.examinationId;
@@ -36,5 +45,25 @@ public class Examination {
 
     public void setTotalScore(int totalScore) {
         this.totalScore = totalScore;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "考试{" +
+                "考试代码:" + examinationId +
+                ",考试名称:" + name +
+                ", 所属课程:" + subject.getName() +
+                ", 时长:" + duration + "分钟" +
+                ", 总分:" + totalScore + "分}";
     }
 }

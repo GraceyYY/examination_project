52bd452d5915d36142dc48a97648f2b0164da58f
Grace <lunay0325@gmail.com>
2019-04-21 17:31:30 +0800

    create a class to store database-related information

---
+++ src/common/Database.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Database {
+    private static final String URL = "jdbc:mysql://localhost:3306/examination_system";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "Ylazy19911217";
+}

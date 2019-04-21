dee98fc36132390c74aedf1b0de719fcdf367ae1
Grace <lunay0325@gmail.com>
2019-04-21 23:45:11 +0800

    overload the constructor to get all students' information

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -7,9 +7,12 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentDao {
     public Student student;
+    public List<Student> allStudents;
 
     public StudentDao(Connection connection, int studentId) {
 
@@ -49,5 +52,24 @@ public class StudentDao {
             e.printStackTrace();
         }
 
+    }
+
+    public StudentDao(Connection connection) {
+        this.allStudents = new ArrayList<>();
+        try (
+                Statement st = Database.getStatement(connection);
+                ResultSet rs = Database.executeSQL(st, "SELECT * FROM student_info")) {
+            while (rs.next()) {
+                int id = rs.getInt("student_id");
+                int userId = rs.getInt("user_id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String sex = rs.getString("sex");
+                this.allStudents.add(new Student(id, userId, name, age, sex));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }

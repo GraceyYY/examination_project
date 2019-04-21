76342580928d5e140c3c91bca5a3faaeaaf515c7
Grace <lunay0325@gmail.com>
2019-04-22 05:06:05 +0800

    modify the url

--- src/tools/Database.java
+++ src/tools/Database.java
@@ -3,7 +3,7 @@ package tools;
 import java.sql.*;
 
 public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/examination_system";
+    private static final String URL = "jdbc:mysql://localhost:3306/examination";
     private static final String NAME = "root";
     private static final String PASSWORD = "Ylazy19911217";
 

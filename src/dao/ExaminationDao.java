bd9d2bc031095a8d6b39a944c24ef2048330fd48
Grace <lunay0325@gmail.com>
2019-04-21 19:35:41 +0800

    overload the constructor so that it can get examination information by subject

--- src/dao/ExaminationDao.java
+++ src/dao/ExaminationDao.java
@@ -4,6 +4,7 @@ import module.Examination;
 import module.Subject;
 import tools.Database;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,6 +23,22 @@ public class ExaminationDao {
                 int duration = rs.getInt("duration");
 
                 this.examination = new Examination(examinationId, subject, duration, score, name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ExaminationDao(Connection connection, Subject subject) {
+        try (Statement st = Database.getStatement(connection);
+             ResultSet rs = Database.executeSQL(st, "SELECT * FROM examinations WHERE subject_id = " + subject.getSubjectId())) {
+            while (rs.next()) {
+                int examinationId = rs.getInt("examination_id");
+                String name = rs.getString("name");
+                int score = rs.getInt("total_score");
+                int duration = rs.getInt("duration");
+
+                this.examination = new Examination(examinationId, subject, score, duration, name);
             }
         } catch (SQLException e) {
             e.printStackTrace();

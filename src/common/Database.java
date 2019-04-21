944cf689e809d584249bea66a176300453b13f9d
Grace <lunay0325@gmail.com>
2019-04-21 17:36:46 +0800

    move Database class to tools package

--- src/common/Database.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-public class Database {
-    private static final String URL = "jdbc:mysql://localhost:3306/examination_system";
-    private static final String NAME = "root";
-    private static final String PASSWORD = "Ylazy19911217";
-}

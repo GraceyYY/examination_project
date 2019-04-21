a5589952625fd3df57cff123aa7a666b7dc4b4f2
Grace <lunay0325@gmail.com>
2019-04-21 19:34:07 +0800

    implement the function to get examination information by eitherexamination_id or subject

---
+++ src/dao/ExaminationDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import module.Examination;
+import module.Subject;
+import tools.Database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ExaminationDao {
+    public Examination examination;
+
+    public ExaminationDao(Connection connection, int examinationId) {
+        try (Statement st = Database.getStatement(connection);
+             ResultSet rs = Database.executeSQL(st, "SELECT * FROM examinations WHERE examination_id = " + examinationId)) {
+            while (rs.next()) {
+                String name = rs.getString("name");
+                Subject subject = new SubjectDao(connection, rs.getInt("subject_id")).subject;
+                int score = rs.getInt("total_score");
+                int duration = rs.getInt("duration");
+
+                this.examination = new Examination(examinationId, subject, duration, score, name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
